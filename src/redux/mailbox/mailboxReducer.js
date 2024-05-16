7c4f6f31a2bfc8438c20bed79ebd61eb53da39d0
RuslanGon <ruslan441006@gmail.com>
2024-05-16 13:00:47 +0300

    wer

--- src/redux/mailbox/mailboxReducer.js
+++ src/redux/mailbox/mailboxReducer.js
@@ -28,28 +28,3 @@ export const { addUser, deleteUser, filterUser } = mailboxSlice.actions;
 
 export const mailboxReducer = mailboxSlice.reducer;
 
-
-
-
-
-
-
-// export const mailboxReduser = (state = INITIAL_STATE, action) => {
-//     switch(action.type){
-//         case 'mailbox/ADD_USER' : {
-//             return {...state,
-//                 users:[...state.users, action.payload]}
-//         }
-//         case 'mailbox/DELETE_USER' : {
-//             return {...state,
-//                 users: state.users.filter((user) => user.id !== action.payload)}
-//         }
-//         case 'mailbox/FILTER_USER' : {
-//             return {...state,
-//                 filter: action.payload}
-//         }
-
-//         default: return state;
-//     }
-
-// };
