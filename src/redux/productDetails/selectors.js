513e84c25fcd971ee48f30d67785167eb2cfbc27
RuslanGon <ruslan441006@gmail.com>
2024-05-17 15:39:27 +0300

    wer

---
+++ src/redux/productDetails/selectors.js
@@ -0,0 +1,13 @@
+export const selectProductDetails = state => state.productDetails.productDetails
+export const selectIsLoading = state => state.productDetails.isLoading
+export const selectIsError = state => state.productDetails.isError
+
+
+
+
+
+
+
+// const productDetails = useSelector(state => state.productDetails.productDetails)
+// const isLoading = useSelector(state => state.productDetails.isLoading)
+// const isError = useSelector(state => state.productDetails.isError)
